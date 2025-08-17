3926805f5dd2322fe2e74a7615ebe5d7c0f5d6c5
@vcastro-dev <vcastro2395@gmail.com>
2025-08-16 22:33:32 -0300

    feat: criando módulo de produto

--- app/api-gateway/src/app.module.ts
+++ app/api-gateway/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 @Module({
-  imports: [OrderModule],
+  imports: [OrderModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- app/api-gateway/src/order/dto/create-order.dto.ts
+++ app/api-gateway/src/order/dto/create-order.dto.ts
@@ -1,13 +1,23 @@
-import { IsArray, IsNotEmpty, IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsArray, IsInt, IsNotEmpty, IsUUID, Min } from 'class-validator';
 
 export class CreateOrderDto {
   @IsUUID()
   userId: string;
 
   @IsArray()
-  @IsNotEmpty()
-  products: {
-    productId: string;
-    quantity: number;
-  }[];
+  @IsNotEmpty({
+    each: true,
+  })
+  @Type(() => ProductDto)
+  products: ProductDto[];
+}
+
+class ProductDto {
+  @IsUUID()
+  productId: string;
+
+  @IsInt()
+  @Min(1)
+  quantity: number;
 }
